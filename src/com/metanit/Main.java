ae34b5410a091d05878f0e359ef5d595702e459e
Yevhen Pyshiev <ypushiev@gfl.geeksforless.net>
2020-11-12 14:28:19 +0200

    test

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,14 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello app!");
+    //    write your code here
+        System.out.println("Welcome to Java!");
+
+        int value=5;
+        System.out.println(value);
+    }
+}
